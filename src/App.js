0203d6f936e5d3d67f650ae042c33a2ba56ec04c
petern160 <peternguyen160@gmail.com>
2019-07-02 19:40:01 -0700

    setup api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+require("dotenv").config();
 
-function App() {
+// console.log(typeof process.env.REACT_APP_APP_ID);
+// console.log(`${process.env.REACT_APP_APP_KEY}` + " warawrawrawr/");
+const APP_ID = process.env.REACT_APP_APP_ID;
+const APP_KEY = process.env.REACT_APP_APP_KEY;
+const App = () => {
+  // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []); // only runs once when page is rendered
+
+  const getRecipes = async () => {
+    // does not come instantly so you have to tell it to wait
+    const response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    // when you have promise add await some data that does not come back instnatly
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
