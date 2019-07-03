06451b2382ee8abadf0fff89fe7e438ca0d75747
petern160 <peternguyen160@gmail.com>
2019-07-03 01:27:37 -0700

    added in css card classes centered display and responsive

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,15 +3,23 @@ import style from "../recipe.module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div className={style.recipe}>
+    <div className="card" id={style.recipes}>
       <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} alt="" />
-      <ol>
+      <p style={{ fontWeight: "800" }}>Calories: {calories}</p>
+      <img
+        className="card-img-top"
+        style={{
+          width: "25rem",
+          height: "20rem"
+        }}
+        src={image}
+        alt=""
+      />
+      <ul>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
-      </ol>
+      </ul>
     </div>
   );
 };
